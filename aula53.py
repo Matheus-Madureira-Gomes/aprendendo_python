8d024e2979b0b9ec3fe35e537c4fea9e5cd4a759
Matheus Madureira Gomes <matheus.madsep@gmail.com>
2026-05-11 13:12:05 -0300

    formatting file sizes

---
+++ aula53.py
@@ -0,0 +1,39 @@
+import math
+import os
+from itertools import count
+
+
+def formata_tamanho(tamanho_em_bytes: int, base: int = 1000) -> str:
+    """Formata um tamanho, de bytes para o tamanho apropriado"""
+
+
+    # Se o tamanho for menor ou igual a 0, 0B.
+    if tamanho_em_bytes <= 0:
+        return "0B"
+
+    # Tupla com os tamanhos
+    #                      0    1     2     3     4     5
+    abreviacao_tamanhos = "B", "KB", "MB", "GB", "TB", "PB"
+    indice_abreviacao_tamanhos = int(math.log(tamanho_em_bytes, base))
+    potencia = base ** indice_abreviacao_tamanhos
+    tamanho_final = tamanho_em_bytes / potencia
+    abreviacao_tamanho = abreviacao_tamanhos[indice_abreviacao_tamanhos]
+    return f'{tamanho_final:.2f} {abreviacao_tamanho}'
+
+
+caminho = os.path.join('/Users', 'mathe', 'barbearia-api')
+counter = count()
+
+for root, dirs, files in os.walk(caminho):
+    the_counter = next(counter)
+    print(the_counter, 'Pasta atual', root)
+
+    for dir_ in dirs:
+        print('  ', the_counter, 'Dir:', dir_)
+
+    for file_ in files:
+        caminho_completo_arquivo = os.path.join(root, file_)
+        # tamanho = os.path.getsize(caminho_completo_arquivo)
+        stats = os.stat(caminho_completo_arquivo)
+        tamanho = stats.st_size
+        print('  ', the_counter, 'FILE:', file_, formata_tamanho(tamanho))
