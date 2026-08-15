0e5f45b6043b05a41dee4e5b4f10e72877995ed8
Matheus Madureira Gomes <matheus.madsep@gmail.com>
2026-08-15 17:46:47 -0300

    sqlite comands

--- aula63/aula63.py
+++ aula63/aula63.py
@@ -4,10 +4,36 @@ from pathlib import Path
 ROOT_DIR = Path(__file__).parent
 DB_NAME = 'db.sqlite3'
 DB_FILE = ROOT_DIR / DB_NAME
+TABLE_NAME = 'customers'
 
 connection = sqlite3.connect(DB_FILE)
 cursor = connection.cursor()
 
+cursor.execute(
+    f'CREATE TABLE IF NOT EXISTS {TABLE_NAME}'
+    '('
+    'id INTEGER PRIMARY KEY AUTOINCREMENT,'
+    'name TEXT,'
+    'weight REAL'
+    ')'
+)
+
+connection.commit()
+
+sql = (f'INSERT INTO {TABLE_NAME} (id, name, weight) '
+      'VALUES(NULL, ?, ?)' )    
+
+nome1 = input('Qual seu nome ?')
+peso1 = input('Qual seu peso ?')
+
+
+cursor.execute(
+    sql, [nome1, peso1]
+)
+connection.commit()
 
 cursor.close()
 connection.close()
+
+
+
